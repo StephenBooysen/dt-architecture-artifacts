b83aac8b1b395c5198d1e0766f604b2c95467feb
Stephen Booysen <stephenrbooysen@gmail.com>
2025-07-31 17:58:29 +0200

    Favicon fixed

--- server/src/components/shared/Layout.jsx
+++ server/src/components/shared/Layout.jsx
@@ -9,7 +9,8 @@ const Layout = ({ children, activeSection, title }) => {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title} - Architecture Artifacts</title>
+        <title>{`${title} - Architecture Artifacts`}</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link 
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
           rel="stylesheet" 
